c2a04f5068e440f7897e560a2d088e1bf2a36fd3
gy_lyons <gy_lyons@163.com>
2019-07-18 16:20:10 +0800

    删除弃用 API

--- js/httpUtils.js
+++ js/httpUtils.js
@@ -3,33 +3,6 @@
 // node 在新的版本中已经简化了HTTP请求，书中createClient方法已经弃用，具体查看官方文档：https://nodejs.org/api/http.html
 exports.get = function (url, callback) {
   url = require('url').parse(url)
-
-  // var hostname = url.hostname,
-  //   port = url.port || 80
-  // var path = url.pathname,
-  //   query = url.query
-  // if (query) {
-  //   path += "?" + query
-  // }
-  // var client = http.createClient(port, hostname)
-  // var request = client.request('GET', path, {
-  //   'Host': hostname
-  // })
-  // request.end()
-
-  // request.on('response', function (response) {
-  //   response.setEncoding('utf8')
-  //   var body = ''
-  //   response.on('data', function (chunk) {
-  //     body += chunk
-  //   })
-  //   response.on('end', function () {
-  //     if (callback) {
-  //       callback(response.statusCode, response.headers, body)
-  //     }
-  //   })
-  // })
-
   var http = require('http')
   http.get(url, (res) => {
     const {
@@ -69,48 +42,7 @@ exports.get = function (url, callback) {
 }
 
 exports.post = function (url, path, params, callback) {
-  // var hostname = url.hostname,
-  //   port = url.port || 80
-  // var path = url.pathname,
-  //   query = url.query
-  // if (query) {
-  //   path += '?' + query
-  // }
-  // var type
-  // if (data == null) {
-  //   data = ''
-  // }
-  // if (data instanceof Buffer) {
-  //   type = 'application/octet-stream'
-  // } else if (typeof data === 'string') {
-  //   type = 'text/plain; charset=UTF-8'
-  // } else if (typeof data === 'object') {
-  //   data = require('querystring').stringify(data)
-  //   type = 'application/x-www-form-urlencoded'
-  // }
-
-  // var client = require('http').createClient(port, hostname)
-  // var request = client.request('POST', path, {
-  //   'Host': hostname,
-  //   'Content-Type': type
-  // })
-  // request.write(data)
-  // request.end()
-  // request.on('response', function (response) {
-  //   response.setEncoding('utf8')
-  //   var body = ''
-  //   response.on('data', function (chunk) {
-  //     body += chunk
-  //   })
-  //   response.on('end', function () {
-  //     if (callback) {
-  //       callback(response.statusCode, response.headers, body)
-  //     }
-  //   })
-  // })
-
   const http = require('http')
-
   const options = {
     hostname: 'localhost',
     port: 8000,
